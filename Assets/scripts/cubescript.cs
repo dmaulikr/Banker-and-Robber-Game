c0d31fd2e75490a88015f5c01d26a5ad4184c588
yunyizhu1 <yunyizhu@mit.edu>
2017-02-22 16:09:41 -0500

    2/22

---
+++ Assets/scripts/cubescript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubescript : MonoBehaviour {
+	public int value; 
+	// Use this for initialization
+	void Start () {
+		value = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/scripts/gamescript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class gamescript : MonoBehaviour {
+	public int storedValue;
+	public int bankerLeft;
+	public int robberGained;
+	public int bombLeft;
+	public int roundLeft;
+	void Start () {
+		storedValue = 0;
+		bankerLeft = 10;
+		robberGained = 0;
+		bombLeft = 3;
+		roundLeft = 7;
+	}
+
+		
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
