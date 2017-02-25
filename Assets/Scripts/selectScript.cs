c0f4bc2dd0901b53a20c5f5d5ceae7311139f031
Audrey Li <audreyli@dhcp-18-189-33-127.dyn.mit.edu>
2017-02-25 17:05:53 -0500

    Implemented click and move for bombs and coins

---
+++ Assets/Scripts/selectScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectScript : MonoBehaviour {
+	
+	bool selected;
+	public Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+		selected = false;
+		target = transform.position;
+	}
+
+	void OnMouseDown () {
+		selected = !selected;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (selected) {
+			if(Input.GetMouseButtonDown(0))
+			{
+				target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				target.z = transform.position.z;
+			}
+			transform.position = Vector3.MoveTowards (transform.position, target, Time.deltaTime * 5);
+		}
+
+	}
+}
